750c4510f1b76a629e664de7ce07d1673e17e804
Rabban <rabban@etra.group>
2025-10-17 11:10:07 +0100

    Update test.php

--- php/public/test.php
+++ php/public/test.php
@@ -1,6 +1,31 @@
 <?php
+$servername = 'db'; // container hostname = service name
+$username   = getenv('MYSQL_USER');
+$password   = getenv('MYSQL_PASSWORD');
+$dbname     = "foodie_prod";
 
-die;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-phpinfo();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Run SELECT query
+$sql = "SELECT * FROM socialmedia_posts LIMIT 2";
+$result = $conn->query($sql);
+
+// Output results
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<pre>";
+        print_r($row);
+        echo "</pre>";
+    }
+} else {
+    echo "No results found.";
+}
+
+$conn->close();
 ?>
